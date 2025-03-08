0cbd537849d0587a8216621a375d40b9eef5ac27
zafarali <zafaralyjamali722@gmail.com>
2025-03-08 22:59:47 +0500

    Initial Commit

---
+++ IdeaSprout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IdeaSprout
+//
+//  Created by Zafar ‎ on 08/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IdeaSprout/IdeaSproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaSproutApp.swift
+//  IdeaSprout
+//
+//  Created by Zafar ‎ on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IdeaSproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
