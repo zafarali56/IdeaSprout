35e11b377abeab98cfafd4f576dbe5e88501e5a2
zafarali56 <zafaralyjamali722@gmail.com>
2025-04-09 12:50:11 +0500

    added permission handler with fetch function to show the photos.

--- IdeaSprout/Core/Create/VIew/fullScreenPhotoGalleryView.swift
+++ IdeaSprout/Core/Create/VIew/fullScreenPhotoGalleryView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import Photos
 struct fullScreenPhotoGalleryView: View {
     @Environment(\.dismiss) private var dismiss
     @Bindable private var viewModel: createViewModel
@@ -14,55 +14,90 @@ struct fullScreenPhotoGalleryView: View {
         self._viewModel = Bindable(wrappedValue:viewModel)
     }
     var body: some View {
-        VStack(spacing:0, content: {
-            ZStack {
-                HStack(content: {
-                    Button(action: {
-                        dismiss()
-                    }, label: {
-                        Image(systemName: "xmark")
-                            .imageScale(.large)
-                            .foregroundStyle(.black)
-                        
+        GeometryReader{ proxy in
+            VStack(spacing:0, content: {
+                ZStack {
+                    HStack(content: {
+                        Button(action: {
+                            dismiss()
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .imageScale(.large)
+                                .foregroundStyle(.black)
+                        })
+                        Spacer()
+                        Button("next", action: {})
+                            .foregroundStyle(.red)
+                            .fontWeight(.semibold)
                     })
-                    Spacer()
-                    Button("next", action: {})
-                        .foregroundStyle(.red)
-                        .fontWeight(.semibold)
-                })
-                
-                
-                Text("All photos")
-                    .font(.headline)
-            }.padding(.horizontal)
-                .padding(.vertical)
-            HStack(spacing: 30, content: {
-                ForEach (viewModel.tabs, id: \.self){tab in
-                    VStack(spacing: 8, content: {
-                        Text(tab)
-                            .foregroundStyle(viewModel.isSelectedTab == tab ? .black : .gray)
-                        Rectangle()
-                            .fill(viewModel.isSelectedTab == tab ? .black : .clear)
-                            .frame(height: 2)
-                        
-                    }).onTapGesture {
-                        viewModel.isSelectedTab = tab
+                    
+                    
+                    Text("All photos")
+                        .font(.headline)
+                }.padding(.horizontal)
+                    .padding(.vertical)
+                HStack(spacing: 30, content: {
+                    ForEach (viewModel.tabs, id: \.self){tab in
+                        VStack(spacing: 8, content: {
+                            Text(tab)
+                                .foregroundStyle(viewModel.isSelectedTab == tab ? .black : .gray)
+                            Rectangle()
+                                .fill(viewModel.isSelectedTab == tab ? .black : .clear)
+                                .frame(height: 2)
+                            
+                        }).onTapGesture {
+                            viewModel.isSelectedTab = tab
+                        }
                     }
+                })
+                ScrollView(content: {
+                    LazyVGrid(columns: viewModel.gridItems,spacing: 1, content: {
+                        ForEach(viewModel.photoAssests , id: \.localIdentifier){assets in
+                            photoThumbnailView(asset: assets, size: proxy.size.width / 3)
+                        }
+                    })
+                }).onAppear {
+                    viewModel.photoPermission()
                 }
+                .scrollIndicators(.hidden)
+                .padding(.horizontal)
+                Spacer()
             })
-            ScrollView(content: {
-                LazyVGrid(columns: viewModel.gridItems,spacing: 1, content: {
-                    ForEach(viewModel.photoAssests , id: \.localIdentifier){assets in
-                        
-                    }
-                })
-            }).scrollIndicators(.hidden)
-            .padding(.horizontal)
-            
+        }
+    }
+}
 
-            
-            Spacer()
+struct photoThumbnailView : View {
+    let asset : PHAsset
+    let size : CGFloat
+    @State private var image: UIImage?
+    private let imageManager = PHCachingImageManager()
+    var body: some View {
+        ZStack( content: {
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: size, height: size)
+                    .clipped()
+            }
         })
+        .onAppear{
+            loadImage()
+        }
+    }
+    func loadImage (){
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .opportunistic
+        options.isNetworkAccessAllowed = true
+        options.resizeMode = .fast
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: size, height: size), contentMode: .aspectFill, options: options){ result, _ in
+            if let image = result {
+                DispatchQueue.main.async{
+                    self.image = image
+                }
+            }
+        }
     }
 }
 
